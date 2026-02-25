876044f7aa29f686f4ad30ece97bb32ef49c6e54
John-Kurwa <mumbajohn848@gmail.com>
2026-02-25 11:01:38 +0300

    First commit

---
+++ app/persp/[id].tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const PerceptionsDetails = () => {
+  return (
+    <View>
+      <Text>Perceptions Details</Text>
+    </View>
+  )
+}
+
+export default PerceptionsDetails
+
